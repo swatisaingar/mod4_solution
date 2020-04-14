09c8a3bedcdd76d04b799169918ef7a8e2497a02
SwatiSaingar <49398992+swatisaingar@users.noreply.github.com>
2020-04-15 01:26:00 +0530

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,8 +1,8 @@
-(function () {
-var helloSpeaker = {};
-var speakWord = "Hello";
-helloSpeaker.speak = function (name) {
-  console.log(speakWord + " " + name);
-}
-this.helloSpeaker = helloSpeaker;
-})();
+(function(window) {
+    var helloSpeaker = new Object();
+    var speakWord = "Hello";
+    helloSpeaker.speak = function(name) {
+        console.log(speakWord + " " + name);
+    };
+    window.helloSpeaker = helloSpeaker;
+})(window);
